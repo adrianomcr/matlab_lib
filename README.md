b124ca088f09cce8e212c0ea1893b5f1622eaeaf
adrianomcr <adrianomcr18@gmail.com>
2019-08-09 15:19:25 -0300

    plot block function

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # matlab_lib
-Useful functions and libraries for Matlab
+This repository contains some useful functions and libraries for Matlab
+
+Files can be obtained with the following command:
+
+``git clone https://github.com/adrianomcr/matlab_lib.git``
+
+
+
+## Installation
+
+After clonning this repository to apath in you computer
+On the Matlab command window type
+
+``addpath('/home/adrianomcr/matlab_lib/')``
+
+``addpath('/home/adrianomcr/matlab_lib/functions')``
+
+``addpath('/home/adrianomcr/matlab_lib/scripts')``
+
+``savepath``
+
+
+
+
+## Scripts
+
+
+
+
+## Functions
+
+- plot_frame:
+
+- plot_cylinder:
---
+++ functions/plot_frame_str.m
@@ -0,0 +1,41 @@
+
+% 
+%function [] = plot_frame_str(H,str,len,wid,fontsize)
+%
+%H - homogeneous transformation
+%str - String with the name of the frame
+%len - Length of the plot
+%wid - Width of the line
+%fontsize - Font size of the string
+%
+%Note: hold should be on. Use hold on before calling this function
+
+%Written by: Adriano Rezende
+
+function [] = plot_frame_str(H,str,len,wid,fontsize)
+% function plot_frame_str(p,q,str,)
+
+
+
+    x = [[0 1]*len;[0 0];[0 0];[1 1]];
+    y = [[0 0];[0 1]*len;[0 0];[1 1]];
+    z = [[0 0];[0 0];[0 1]*len;[1 1]];
+
+    z_str = [0;0;1.5*len;1];
+
+    x = H*x; x(4,:) = [];
+    y = H*y; y(4,:) = [];
+    z = H*z; z(4,:) = [];
+    z_str = H*z_str; z_str(4,:) = [];
+
+
+
+    plot3(x(1,:),x(2,:),x(3,:),'r','LineWidth',wid)
+    plot3(y(1,:),y(2,:),y(3,:),'g','LineWidth',wid)
+    plot3(z(1,:),z(2,:),z(3,:),'b','LineWidth',wid)
+
+    text(z_str(1,:),z_str(2,:),z_str(3,:),str,'FontSize',fontsize,'interpreter','latex')
+
+
+
+end
