c22914444f00baaed431842ae5e07a8fafdfaead
adrianomcr <adrianomcr18@gmail.com>
2020-06-25 18:08:33 -0300

    adding option for noncausal filter

--- functions/average_filter.m
+++ functions/average_filter.m
@@ -3,30 +3,52 @@
 
 
 
-%function y = average_filter(x,n)
+%function y = average_filter(x,n,c)
 %
 %Input:
 %x: vector with the signal to be filtered, each line is a signal
 %n: number of elements to compute the average
+%c: flag to set causal filter (1) or non caulsal filter
 %
 %Output:
 %y: filtered vector
 %
 
-function y = average_filter(x,n)
+function y = average_filter(x,n,c)
 
+
+    
 
     if (n > length(x(1,:)))
         error('Value of is bigger than the number of elements on the vector')
     end
     
-    y = x;
-    for k = 2:1:n-1
-        y(:,k) = mean(x(:,1:k)')';
+
+    if(c == 1)
+        y = x;
+        for k = 2:1:n-1
+            y(:,k) = mean(x(:,1:k)')';
+        end
+        for k = n:1:length(x(1,:))
+            y(:,k) = mean(x(:,k-n+1:k)')';
+        end
+    else
+        n = round(n/2);
+        y = x;
+        
+        for k = 2:1:n-1
+            y(:,k) = mean(x(:,1:k)')';
+        end
+        for k = n:1:length(x(1,:))-n
+            y(:,k) = mean(x(:,k-n+1:k+n)')';
+        end
+        for k = length(x(1,:))-n+1:1:length(x(1,:))
+            y(:,k) = mean(x(:,k:end)')';
+        end
     end
-    for k = n:1:length(x(1,:))
-        y(:,k) = mean(x(:,k-n+1:k)')';
-    end
+    
+
+
     
 
 
